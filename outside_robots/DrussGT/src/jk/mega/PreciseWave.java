5924edf22e04faeccfbd092b0d2dc4cb8a11b973
alexey.jdev.zhidkov <alexey.jdev.zhidkov@338115b6-3490-f9c3-7999-2ebf30ad46d8>
2011-03-04 09:32:23 +0000

    other bots initial commit

---
+++ outside_robots/DrussGT/src/jk/mega/PreciseWave.java
@@ -0,0 +1,9 @@
+package jk.mega;
+
+import java.awt.geom.Point2D;
+
+public class PreciseWave {
+    public double distanceTraveled;
+    public double bulletVelocity;
+    public Point2D.Double fireLocation;
+}
---
+++ outside_robots/WaveSerpent/src/kc/serpent/movement/MovementWave.java
@@ -0,0 +1,10 @@
+package kc.serpent.movement;
+
+import kc.serpent.utils.*;
+
+public class MovementWave extends Wave {
+    public double power;
+    public int impactTime;
+    public double weight;
+    public boolean hasLoggedVisit;
+}
