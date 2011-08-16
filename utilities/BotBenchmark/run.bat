e311209e86cac7ba4f2549ec491e3a93c889cc3c
alexey.jdev.zhidkov <alexey.jdev.zhidkov@338115b6-3490-f9c3-7999-2ebf30ad46d8>
2011-08-16 04:48:34 +0000

    delete utilities from trunk

--- utilities/BotBenchmark/run.bat
+++
@@ -1,3 +0,0 @@
-set ROBOCODE_HOME=E:\lexx\games\rc\libs
-
-java -Xmx512M -classpath ".\bin;%ROBOCODE_HOME%\robocode.repository-1.7.1.3.jar;%ROBOCODE_HOME%\robocode.ui-1.7.1.3.jar;%ROBOCODE_HOME%\robocode.core-1.7.1.3.jar;%ROBOCODE_HOME%\robocode.battle-1.7.1.3.jar;%ROBOCODE_HOME%\robocode.jar;%ROBOCODE_HOME%\picocontainer-2.6.jar" ru.jdev.robocode.benchmark.BotBenchmark
